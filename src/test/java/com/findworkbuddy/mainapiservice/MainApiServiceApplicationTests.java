96e7b8e2e8948ddcd30c040c239f34af4b078beb
Kiril Raykov (V) <kraykov@shutterfly.com>
2019-04-16 23:13:55 +0300

    Initial commit

---
+++ src/test/java/com/findworkbuddy/mainapiservice/MainApiServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.findworkbuddy.mainapiservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MainApiServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
