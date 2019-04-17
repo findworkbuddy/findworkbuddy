1adb14de9ebc24cdc705fc166070a7bc905b8c46
Ditsinski <ditsinski@shutterfly.com>
2019-04-17 16:11:53 +0300

    Addin swagger implementation + 1 ping endpoint

--- src/main/java/com/findworkbuddy/mainapiservice/Application.java
+++ src/main/java/com/findworkbuddy/mainapiservice/Application.java
@@ -1,13 +1,16 @@
 package com.findworkbuddy.mainapiservice;
 
+import com.findworkbuddy.mainapiservice.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SwaggerConfig.class)
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args).setId("Find work buddy");
 	}
 
 }
