04418a6dd2190ec4964fcb539d8afdcec8184262
kirilraykov <kirilraykov@gmail.com>
2019-06-09 00:19:32 +0300

    Refactor

--- src/main/java/com/findworkbuddy/mainapiservice/services/admin/resource/AdminController.java
+++ src/main/java/com/findworkbuddy/mainapiservice/services/admin/resource/AdminController.java
@@ -1,6 +1,9 @@
 package com.findworkbuddy.mainapiservice.services.admin.resource;
 
+import com.findworkbuddy.mainapiservice.model.CustomUserDetails;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,13 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
-@RequestMapping(value = "secured/v1", method = RequestMethod.GET)
-@ResponseStatus(HttpStatus.OK)
+@RequestMapping(value = "secured/v1")
 @ApiOperation(value = "Health check")
-public class AdminResourceImpl implements AdminResource{
+public class AdminResourceImpl {
 
     @GetMapping(value = "ping")
-    public String ping() {
+    public String ping(@AuthenticationPrincipal CustomUserDetails userDetails) {
             return("pong");
     }
 }
